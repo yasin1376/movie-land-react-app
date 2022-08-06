ff0e2c533e4180926747802a5dbd1b4f95b537fa
Yasin <yasinm289@gmail.com>
2022-08-06 11:26:24 +0430

    some changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import MovieCard from './MovieCard';
 import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=20d79642';
 
-const movie1 = {
-  "Title": "Italian Spiderman",
-  "Year": "2007",
-  "imdbID": "tt2705436",
-  "Type": "movie",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
-};
-
 const App = () => {
+
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async (title) => {
       const response = await fetch(`${API_URL}&s=${title}`);
       const data = await response.json();
-      console.log(data);
+      setMovies(data.Search);
   };
 
   useEffect(() => {
@@ -32,9 +26,19 @@ const App = () => {
         <input placeholder='Search for moviews' value='Superman' onChange={() => {}} />
         <img src={SearchIcon} alt='search' onClick={() => {}} />
       </div>
-      <div className='container'>
-        <MovieCard movie1={movie1} />
-      </div>
+      {
+        movies.length > 0 ?
+        ( <div className='container'>
+          {movies.map((movie)=> <MovieCard movie={movie} />)}
+        
+      </div>) : (
+        <div className='empty'>
+          <h2>No movies found</h2>
+        </div>
+      )
+
+      }
+     
     </div>
   );
 };
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-const MovieCard = ({movie1})=> {
+const MovieCard = ({movie})=> {
     return (
         <div className='movie'>
           <div>
-            <p>{movie1.Year}</p>
+            <p>{movie.Year}</p>
           </div>
           <div>
-            <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title} />
+            <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc='} alt={movie.Title} />
           </div>
           <div>
-            <span>{movie1.Type}</span>
-            <h3>{movie1.Title}</h3>
+            <span>{movie.Type}</span>
+            <h3>{movie.Title}</h3>
           </div>
         </div>
     );
